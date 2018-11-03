f982cf904d5862d470e2058dae60f19f5b0f3c18
Utku Gultopu <ugultopu@gmail.com>
2018-11-03 03:50:53 -0300

    Insert platforms to support blocks

--- structure.py
+++ structure.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from configparser import ConfigParser
 
 from shapely.geometry import Polygon
@@ -46,37 +47,44 @@ class Structure:
     def __init__(self,
                  level_path,
                  shape,
-                 p_block,
-                 num_p_blocks_on_x_axis):
+                 primary_block,
+                 platform_block,
+                 num_primary_blocks_on_x_axis):
         self.level_path = level_path
         self.shape = shape
-        self.p_block = p_block
-        self.num_p_blocks_on_x_axis = num_p_blocks_on_x_axis
-        self.p_block_factor = self.get_principal_block_factor(num_p_blocks_on_x_axis)
-        self.factored_p_block_width, self.factored_p_block_height = self.get_factored_principal_block_dimensions()
-        self.num_p_blocks_on_y_axis = self.get_number_of_instances_required_to_cover_distance(self.get_shape_height(self.shape), self.factored_p_block_height)
-        self.blocks = self.transpose_and_invert_blocks(self.get_blocks())
+        self.primary_block = primary_block
+        self.platform_block = platform_block
+        self.num_primary_blocks_on_x_axis = num_primary_blocks_on_x_axis
+        self.primary_block_factor = self.get_primary_block_factor(num_primary_blocks_on_x_axis)
+        self.factored_primary_block_width, self.factored_primary_block_height = self.get_factored_primary_block_dimensions()
+        self.num_primary_blocks_on_y_axis = self.get_number_of_instances_required_to_cover_distance(self.get_shape_height(self.shape), self.factored_primary_block_height)
+        self.original_blocks = self.get_blocks()
+        self.blocks = self.transpose_and_invert_blocks(self.original_blocks)
+        # This is to start from bottom row and go towards the top row, instead
+        # of vice-versa.
+        self.original_blocks = self.original_blocks[::-1]
+        self.platforms = self.get_platforms()
 
 
-    def get_principal_block_factor(self, num_p_blocks):
+    def get_primary_block_factor(self, num_primary_blocks):
         '''Normally every block has a width and height. However, since we want to
-        decide on the number of principal blocks that will exist on an axis of the
-        structure, we want to proportionate the principal block dimensions according
+        decide on the number of primary blocks that will exist on an axis of the
+        structure, we want to proportionate the primary block dimensions according
         to this number. Hence, we get the multiplier needed to get this number.'''
         shape_width = self.get_shape_width(self.shape)
-        target_p_block_width = shape_width / num_p_blocks
-        return target_p_block_width / self.p_block.width
+        target_primary_block_width = shape_width / num_primary_blocks
+        return target_primary_block_width / self.primary_block.width
 
 
-    def get_factored_principal_block_dimensions(self):
-        return (self.p_block.width * self.p_block_factor,
-                self.p_block.height * self.p_block_factor)
+    def get_factored_primary_block_dimensions(self):
+        return (self.primary_block.width * self.primary_block_factor,
+                self.primary_block.height * self.primary_block_factor)
 
 
     def is_tile_mostly_in_shape(self, tile):
         '''We designate an area for the structure and we partition it into small,
         equal tiles. The tiles can be of any size and shape but for simplicity, they
-        are usually square. The tiles represent the "principal blocks" that are
+        are usually square. The tiles represent the "primary blocks" that are
         going to form the structure in the output.
 
         Then, in order to determine whether there should be a block present on a
@@ -92,15 +100,15 @@ class Structure:
         # Blocks is a boolean array indicating whether or not there is a block
         # in the indicated index.
         blocks = []
-        for row in range(self.num_p_blocks_on_y_axis):
+        for row in range(self.num_primary_blocks_on_y_axis):
             blocks_in_row = []
-            y = self.shape.bounds[1] + row * self.factored_p_block_height
-            for column in range(self.num_p_blocks_on_x_axis):
-                x = self.shape.bounds[0] + column * self.factored_p_block_width
+            y = self.shape.bounds[1] + row * self.factored_primary_block_height
+            for column in range(self.num_primary_blocks_on_x_axis):
+                x = self.shape.bounds[0] + column * self.factored_primary_block_width
                 tile = Polygon([(x,y),
-                                (x + self.factored_p_block_width, y),
-                                (x + self.factored_p_block_width, y + self.factored_p_block_height),
-                                (x, y + self.factored_p_block_height)])
+                                (x + self.factored_primary_block_width, y),
+                                (x + self.factored_primary_block_width, y + self.factored_primary_block_height),
+                                (x, y + self.factored_primary_block_height)])
                 if self.is_tile_mostly_in_shape(tile):
                     blocks_in_row.append(True)
                 else:
@@ -109,22 +117,87 @@ class Structure:
         return blocks
 
 
-    def get_xml_elements_for_primary_blocks(self):
-        elements = ''
+    def get_platforms(self):
+        '''In order to support blocks without anything underneath, we need to
+        insert platforms.
+        '''
+        platforms = set()
+        for column in self.blocks:
+            last_element_is_a_gap = True
+            for row, tile in reversed(list(enumerate(column))):
+                if tile:
+                    last_element_is_a_gap = False
+                else:
+                    if not last_element_is_a_gap:
+                        platforms.add(row)
+                    last_element_is_a_gap = True
+        return platforms
+
+
+    def get_block_height(self, block_type, index, platforms):
+        number_of_platforms = bisect_left(sorted(self.platforms), index)
+        total_platform_height = number_of_platforms * self.platform_block.height
+        total_primary_block_height = index * self.primary_block.height
+        if block_type is self.platform_block:
+            total_primary_block_height += self.primary_block.height
+        positioning_distance = block_type.height / 2
+        return (GROUND_HEIGHT
+              + total_platform_height
+              + total_primary_block_height
+              + positioning_distance)
+
+
+    def get_lateral_distances_for_platform_blocks(self, index):
+        lateral_distances = []
+        # WARNING This method assumes that the empty blocks exists only on the
+        # beginning and end of a row. If any empty block exists after a
+        # non-empty block in the row, this calculation breaks down.
+        number_of_empty_blocks_before_the_first_non_empty_block = self.original_blocks[index + 1].index(True)
+        number_of_empty_blocks_after_the_last_non_empty_block = self.original_blocks[index + 1][::-1].index(True)
+        number_of_primary_blocks_to_cover = (len(self.original_blocks[index + 1])
+                                           - number_of_empty_blocks_before_the_first_non_empty_block
+                                           - number_of_empty_blocks_after_the_last_non_empty_block)
+        platform_center_distance = ((number_of_empty_blocks_before_the_first_non_empty_block
+                                   + number_of_primary_blocks_to_cover / 2)) * self.primary_block.width
+        distance_to_cover = number_of_primary_blocks_to_cover * self.primary_block.width
+        number_of_platform_blocks = self.get_number_of_instances_required_to_cover_distance(distance_to_cover, self.platform_block.width)
+        if number_of_platform_blocks % 2 is 0:
+            lateral_distances.append(platform_center_distance - self.platform_block.width / 2)
+            lateral_distances.append(platform_center_distance + self.platform_block.width / 2)
+            number_of_platform_blocks -= 2
+        else:
+            lateral_distances.append(platform_center_distance)
+            number_of_platform_blocks -= 1
+        for i in range(int(number_of_platform_blocks / 2)):
+            lateral_distances = [lateral_distances[0] - self.platform_block.width] + lateral_distances
+            lateral_distances.append(lateral_distances[-1] + self.platform_block.width)
+        return lateral_distances
+
+
+    def get_xml_elements(self):
+        primary_block_elements = ''
         for column in range(len(self.blocks)):
             for row in range(len(self.blocks[column])):
                 if self.blocks[column][row]:
-                    elements += BLOCK_STRING.format(self.p_block.xml_element_name,
-                                                    'stone',
-                                                    column * self.p_block.width + self.p_block.width / 2,
-                                                    GROUND_HEIGHT + row * self.p_block.height + self.p_block.height / 2,
-                                                    0)
-        return elements
+                    primary_block_elements += BLOCK_STRING.format(self.primary_block.xml_element_name,
+                                                                  'stone',
+                                                                  column * self.primary_block.width + self.primary_block.width / 2,
+                                                                  self.get_block_height(self.primary_block, row, self.platforms),
+                                                                  0)
+        platform_block_elements = ''
+        for platform in self.platforms:
+            for lateral_distance in self.get_lateral_distances_for_platform_blocks(platform):
+                platform_block_elements += BLOCK_STRING.format(self.platform_block.xml_element_name,
+                                                               'stone',
+                                                               lateral_distance,
+                                                               self.get_block_height(self.platform_block, platform, self.platforms),
+                                                               0)
+        return primary_block_elements + platform_block_elements
 
 
     def construct_structure(self):
         with open(self.level_path, 'w') as level_file:
-            level_file.write(LEVEL_TEMPLATE.strip().format(self.get_xml_elements_for_primary_blocks()))
+            level_file.write(LEVEL_TEMPLATE.strip().format(self.get_xml_elements()))
 
 
 if __name__ == '__main__':
@@ -132,6 +205,7 @@ if __name__ == '__main__':
     config.read('config.ini')
 
     structure = Structure(config.get('DEFAULT', 'LevelPath'),
-                          Polygon([(0,0),(0,10),(10,10),(10,0)]),
+                          Polygon([(0,0),(20,0),(10,20)]),
                           BLOCK_REGISTRY[config.get('DEFAULT', 'PrimaryBlock')],
-                          10).construct_structure()
+                          BLOCK_REGISTRY['long_rectangle'],
+                          int(config.get('DEFAULT', 'NumberOfPrimaryBlocksOnXAxis'))).construct_structure()
