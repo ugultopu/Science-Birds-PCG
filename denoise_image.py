b9da0c81ab1b7056f8cb24a77d17dc53ccea2b05
Utku Gultopu <ugultopu@gmail.com>
2018-12-16 18:59:43 -0400

    Apply both Gaussian filter and median filter

--- denoise_image.py
+++ denoise_image.py
@@ -1,6 +1,6 @@
 from sys import argv
 
 from imageio import imread, imwrite
-from scipy.ndimage import median_filter
+from scipy.ndimage import gaussian_filter, median_filter
 
-imwrite(argv[2], median_filter(imread(argv[1]), 10))
+imwrite(argv[2], gaussian_filter(median_filter(imread(argv[1]), 10), 10))
